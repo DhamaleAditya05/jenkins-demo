a43386fd6dae3c9b0dade8dc865ea3463af31580
DhamaleAditya05 <128235954+DhamaleAditya05@users.noreply.github.com>
2026-05-03 17:50:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-import requests
+from flask import Flask
 
-print("Hello from DevOps 🚀")
-print("Requests library imported successfully!")
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from DevOps Web App 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
